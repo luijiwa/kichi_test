e8faeec3a75b5dd4bea1a90e598ea7342355d32f
luijiwa <aratar006@gmail.com>
2023-10-18 12:29:47 +0700

    Добавить sqflite

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:lichi_test/domain/services/database_services.dart';
 import 'package:lichi_test/widgets/my_app.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final databaseServices = DatabaseServices();
+  await databaseServices.open();
+
   runApp(const MyApp());
 }
