549b158387bd31bc49cc10b2462b05f8dfcbe972
luijiwa <aratar006@gmail.com>
2023-10-18 12:30:16 +0700

    Добавить общие стили текста

---
+++ lib/widgets/styles/custom_text_style.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class CustomTextStyle {
+  static const TextStyle title1 = TextStyle(
+    fontSize: 30,
+    letterSpacing: -0.408,
+  );
+  static const TextStyle title2 = TextStyle(
+    fontSize: 25,
+    fontWeight: FontWeight.w400,
+    letterSpacing: -0.408,
+  );
+  static const TextStyle title3 = TextStyle(
+    fontSize: 20,
+    letterSpacing: -0.408,
+  );
+  static const TextStyle title4 = TextStyle(
+    fontSize: 16,
+    letterSpacing: -0.408,
+  );
+  static const TextStyle title5 = TextStyle(
+    fontSize: 14,
+    letterSpacing: -0.408,
+    fontWeight: FontWeight.w300,
+  );
+  static const TextStyle paragraph = TextStyle(
+    fontSize: 13,
+    letterSpacing: -0.408,
+    fontWeight: FontWeight.w300,
+  );
+
+//Не заданные изначально:
+  static const TextStyle cartTitle = TextStyle(
+    fontSize: 15,
+    letterSpacing: -0.408,
+  );
+  static const TextStyle cartItemStyle = TextStyle(
+    fontSize: 14,
+  );
+  static const TextStyle boldAndSixteen = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 16,
+  );
+  static const TextStyle cartItemAddStyle = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 16,
+  );
+  static const TextStyle cartPriceStyle1 = TextStyle(
+    fontFamily: 'Rubik',
+    fontSize: 14,
+    letterSpacing: -0.408,
+    fontWeight: FontWeight.w300,
+  );
+  static const TextStyle cartPriceStyle2 = TextStyle(
+    fontFamily: 'Rubik',
+    fontSize: 30,
+    letterSpacing: -0.408,
+  );
+  static const TextStyle succesAddToCart = TextStyle(
+    fontSize: 20,
+    letterSpacing: -0.408,
+    fontWeight: FontWeight.w300,
+  );
+  static const TextStyle succesAddToCartItemName = TextStyle(
+    fontSize: 16,
+    letterSpacing: -0.408,
+    fontWeight: FontWeight.w300,
+  );
+}
