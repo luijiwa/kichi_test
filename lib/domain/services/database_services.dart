845804c5d8ca39b839d7144f1a4aacee8036363e
luijiwa <aratar006@gmail.com>
2023-10-21 13:51:27 +0700

    Настроить работу sqflite

--- lib/domain/services/database_services.dart
+++ lib/domain/services/database_services.dart
@@ -1,32 +1,141 @@
 import 'package:lichi_test/domain/entity/cart_items.dart';
+import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DatabaseServices {
-  Database? _db;
+  static final DatabaseServices instance = DatabaseServices._init();
 
-  Future<void> open() async {
-    _db = await openDatabase('cart.db');
+  static Database? _database;
+
+  DatabaseServices._init();
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+
+    _database = await _initDB('cart.db');
+    return _database!;
   }
 
-  Future<void> close() async {
-    await _db?.close();
+  Future<Database> _initDB(String filePath) async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, filePath);
+
+    return await openDatabase(path, version: 1, onCreate: _createDB);
   }
 
-  Future<List<CartItems>> getCartItems() async {
-    final List<Map<String, Object?>>? results = await _db?.query('cart_items');
-    return results?.map((result) => CartItems.fromJson(result)).toList() ?? [];
+  Future _createDB(Database db, int version) async {
+    const idType = 'INTEGER PRIMARY KEY';
+    const textType = 'TEXT NOT NULL';
+    const integerType = 'INTEGER NOT NULL';
+
+    await db.execute('''
+CREATE TABLE $tableCartItems ( 
+  ${CartItemsFields.id} $idType, 
+  ${CartItemsFields.name} $textType,
+  ${CartItemsFields.image} $textType,
+  ${CartItemsFields.price} $integerType,
+  ${CartItemsFields.color} $textType,
+  ${CartItemsFields.size} $textType, 
+  ${CartItemsFields.quantity} $integerType
+  )
+''');
   }
 
-  Future<void> addCartItem(CartItems cartItem) async {
-    await _db?.insert('cart_items', cartItem.toJson());
+  Future<CartItems?> createCartItem(CartItems cartItem) async {
+    final db = await instance.database;
+
+    try {
+      final cartItemExist = await readCartItem(cartItem.id);
+      if (cartItemExist != null) {
+        final updatedCartItem = cartItemExist.copyWith(
+          quantity: cartItemExist.quantity + 1,
+        );
+        await update(updatedCartItem);
+      } else {
+        final id = await db.insert(tableCartItems, cartItem.toJson());
+        return cartItem.copyWith(id: id);
+      }
+    } catch (e) {
+      print('Что-то навернулось: $e');
+      return cartItem;
+    }
+    return null;
   }
 
-  Future<void> updateCartItem(CartItems cartItem) async {
-    await _db?.update('cart_items', cartItem.toJson(),
-        where: 'id = ?', whereArgs: [cartItem.aproduct.id]);
+  Future<List<CartItems>> readAllCartItems() async {
+    final db = await instance.database;
+
+    final result = await db.query(tableCartItems);
+    return result.map((json) => CartItems.fromJson(json)).toList();
   }
 
-  Future<void> removeCartItem(int id) async {
-    await _db?.delete('cart_items', where: 'id = ?', whereArgs: [id]);
+  Future<CartItems?> readCartItem(int id) async {
+    final db = await instance.database;
+
+    final maps = await db.query(
+      tableCartItems,
+      columns: CartItemsFields.values,
+      where: '${CartItemsFields.id} = ?',
+      whereArgs: [id],
+    );
+
+    if (maps.isNotEmpty) {
+      return CartItems.fromJson(maps.first);
+    } else {
+      return null;
+    }
+  }
+
+  Future<int> update(CartItems cartItem) async {
+    final db = await instance.database;
+
+    return db.update(
+      tableCartItems,
+      cartItem.toJson(),
+      where: '${CartItemsFields.id} = ?',
+      whereArgs: [cartItem.id],
+    );
+  }
+
+  Future<int> delete(int id) async {
+    final db = await instance.database;
+    final cartItem = await readCartItem(id);
+
+    if (cartItem == null) return 0;
+
+    if (cartItem.quantity > 1) {
+      return await db.update(
+        tableCartItems,
+        {'quantity': cartItem.quantity - 1},
+        where: 'id = ?',
+        whereArgs: [id],
+      );
+    } else {
+      return await db.delete(
+        tableCartItems,
+        where: '${CartItemsFields.id} = ?',
+        whereArgs: [id],
+      );
+    }
+  }
+
+  Future<int> addOne(int id) async {
+    final db = await instance.database;
+    final cartItem = await readCartItem(id);
+
+    if (cartItem == null) return 0;
+
+    return await db.update(
+      tableCartItems,
+      {'quantity': cartItem.quantity + 1},
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+
+  Future close() async {
+    final db = await instance.database;
+
+    db.close();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:lichi_test/domain/services/database_services.dart';
 import 'package:lichi_test/widgets/my_app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final databaseServices = DatabaseServices();
-  await databaseServices.open();
 
   runApp(const MyApp());
 }
